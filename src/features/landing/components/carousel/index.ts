d51c65c1edc041cede1be243147a61616fb48d9c
kimjhyun0627 <kimjhyun0627@gmail.com>
2025-11-22 06:28:31 +0900

    refactor: 기존 폴더 삭제

--- src/features/landing/components/carousel/index.ts
+++
@@ -1,4 +0,0 @@
-export { CarouselButton } from './CarouselNavigation';
-export { CarouselIndicators } from './CarouselIndicators';
-export { default as CarouselCard } from './CarouselCard';
-export { CardImage } from './CardImage';
